d828823c08446b59dc8b6cccccc3440fdfe36c4f
Angel Yordanov <yordanov_angel@yahoo.com>
2017-05-01 01:45:04 +0300

    basic layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes: Recipe[]=[
+  new Recipe('Eggs Scrambled','You need two eggs and smile.',
+    'http://assets.epicurious.com/photos/57b35f844924889253994109/master/pass/scrambled-eggs.jpg'),
+  new Recipe('Eggs Scrambled','You need two eggs and smile.',
+    'http://assets.epicurious.com/photos/57b35f844924889253994109/master/pass/scrambled-eggs.jpg')
+];
   constructor() { }
 
   ngOnInit() {
